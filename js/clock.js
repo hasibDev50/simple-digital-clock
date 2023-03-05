9b2bf432b933bd465ca5cb8dd83e1402cb523ba1
hasibDev50 <hasibur.dev50@gmail.com>
2023-03-05 16:42:27 +0600

    showing time on the display

--- js/clock.js
+++ js/clock.js
@@ -1,1 +1,13 @@
-console.log("clock connected");
+const displayTime = () => {
+  let dateTime = new Date();
+  let hrs = dateTime.getHours();
+  let min = dateTime.getMinutes();
+  let sec = dateTime.getSeconds();
+  let session = document.getElementById("session");
+
+  document.getElementById("hours").innerHTML = hrs;
+  document.getElementById("minutes").innerHTML = min;
+  document.getElementById("seconds").innerHTML = sec;
+};
+
+setInterval(displayTime, 10);
